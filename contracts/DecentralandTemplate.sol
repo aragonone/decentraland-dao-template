c9d83c5b7bf0ffd5e555592bf6ca6fb70e631084
Alejandro J. Santander <palebluedot@gmail.com>
2019-09-03 15:25:20 -0300

    Template and tests bootstrapped, deploying token

--- contracts/DecentralandTemplate.sol
+++ contracts/DecentralandTemplate.sol
@@ -1,9 +1,14 @@
 pragma solidity 0.4.24;
 
+import "@aragon/templates-shared/contracts/TokenCache.sol";
 import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 
 
-contract DecentralandTemplate is BaseTemplate {
+contract DecentralandTemplate is BaseTemplate, TokenCache {
+
+    bool constant private TOKEN_TRANSFERABLE = true;
+    uint8 constant private TOKEN_DECIMALS = uint8(18);
+    uint256 constant private TOKEN_MAX_PER_ACCOUNT = uint256(0);
 
     constructor(DAOFactory _daoFactory, ENS _ens, MiniMeTokenFactory _miniMeFactory, IFIFSResolvingRegistrar _aragonID)
         BaseTemplate(_daoFactory, _ens, _miniMeFactory, _aragonID)
@@ -13,12 +18,20 @@ contract DecentralandTemplate is BaseTemplate {
         _ensureMiniMeFactoryIsValid(_miniMeFactory);
     }
 
+    function newToken(string memory _name, string memory _symbol) public returns (MiniMeToken) {
+        MiniMeToken token = _createToken(_name, _symbol, TOKEN_DECIMALS);
+        _cacheToken(token, msg.sender);
+        return token;
+    }
+
     function newInstance(string memory _id) public {
         // TODO: Uncomment when updated to @aragon/templates-shared 1.0.0-rc.2
         // _validateId(_id);
 
         (Kernel dao,) = _createDAO();
 
+        // TODO: Transfer to voting.
+        _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, address(this));
         _registerID(_id, dao);
     }
 }
--- test/template.test.js
+++ test/template.test.js
@@ -1,16 +1,24 @@
+const encodeCall = require('@aragon/templates-shared/helpers/encodeCall')
+const assertRevert = require('@aragon/templates-shared/helpers/assertRevert')(web3)
+
 const { hash: namehash } = require('eth-ens-namehash')
 const { randomId } = require('@aragon/templates-shared/helpers/aragonId')
 const { getEventArgument } = require('@aragon/test-helpers/events')
 const { deployedAddresses } = require('@aragon/templates-shared/lib/arapp-file')(web3)
 
+const DecentralandTemplate = artifacts.require('DecentralandTemplate')
+
 const ENS = artifacts.require('ENS')
 const ACL = artifacts.require('ACL')
 const Kernel = artifacts.require('Kernel')
+const MiniMeToken = artifacts.require('MiniMeToken')
 const PublicResolver = artifacts.require('PublicResolver')
-const DecentralandTemplate = artifacts.require('DecentralandTemplate')
 
-contract('DecentralandTemplate', ([owner]) => {
-  let dao, acl, template, ens, daoID
+contract('DecentralandTemplate', ([_, owner]) => {
+  let daoID, template, dao, acl, ens
+
+  const TOKEN_NAME = 'Decentraland Token'
+  const TOKEN_SYMBOL = 'DCL'
 
   before('fetch template and ENS', async () => {
     const { registry, address } = await deployedAddresses()
@@ -18,16 +26,75 @@ contract('DecentralandTemplate', ([owner]) => {
     template = DecentralandTemplate.at(address)
   })
 
-  before('create and initialize DAO', async () => {
-    daoID = randomId()
-    const instanceReceipt = await template.newInstance(daoID)
+  const newInstance = (...params) => {
+    const lastParam = params[params.length - 1]
+    const txParams = (!Array.isArray(lastParam) && typeof lastParam === 'object') ? params.pop() : {}
+    const newInstanceFn = DecentralandTemplate.abi.find(({ name, inputs }) => name === 'newInstance' && inputs.length === params.length)
+    return template.sendTransaction(encodeCall(newInstanceFn, params, txParams))
+  }
+
+  const loadDAO = async (tokenReceipt, instanceReceipt) => {
     dao = Kernel.at(getEventArgument(instanceReceipt, 'DeployDao', 'dao'))
+    token = MiniMeToken.at(getEventArgument(tokenReceipt, 'DeployToken', 'token'))
     acl = ACL.at(await dao.acl())
+
+    assert.equal(dao.address, getEventArgument(instanceReceipt, 'SetupDao', 'dao'), 'should have emitted a SetupDao event')
+  }
+
+  const itSetupsDAOCorrectly = () => {
+    it('registers a new DAO on ENS', async () => {
+      const aragonIdNameHash = namehash(`${daoID}.aragonid.eth`)
+      const resolvedAddress = await PublicResolver.at(await ens.resolver(aragonIdNameHash)).addr(aragonIdNameHash)
+      assert.equal(resolvedAddress, dao.address, 'aragonId ENS name does not match')
+    })
+
+    it('creates a new token', async () => {
+      assert.equal(await token.name(), TOKEN_NAME)
+      assert.equal(await token.symbol(), TOKEN_SYMBOL)
+      assert.equal(await token.transfersEnabled(), true)
+      assert.equal((await token.decimals()).toString(), 18)
+    })
+  }
+
+  context('when creating instances with a single transaction', () => {
+    context('when the creation fails', () => {})
+    context('when the creation succeeds', () => {})
   })
 
-  it('registers a new DAO on ENS', async () => {
-    const aragonIdNameHash = namehash(`${daoID}.aragonid.eth`)
-    const resolvedAddress = await PublicResolver.at(await ens.resolver(aragonIdNameHash)).addr(aragonIdNameHash)
-    assert.equal(resolvedAddress, dao.address, 'aragonId ENS name does not match')
+  context('when creating instances with separate transactions', () => {
+    context('when the creation fails', () => {
+      context('a token was not created before creating the instance', () => {})
+    })
+
+    context('when the creation succeeds', () => {
+      const itCostsUpTo = (expectedDaoCreationCost) => {
+        const expectedTokenCreationCost = 1.8e6
+        const expectedTotalCost = expectedTokenCreationCost + expectedDaoCreationCost
+
+        it(`gas costs must be up to ~${expectedTotalCost} gas`, async () => {
+          const tokenCreationCost = tokenReceipt.receipt.gasUsed
+          assert.isAtMost(tokenCreationCost, expectedTokenCreationCost, `token creation call should cost up to ${tokenCreationCost} gas`)
+
+          const daoCreationCost = instanceReceipt.receipt.gasUsed
+          assert.isAtMost(daoCreationCost, expectedDaoCreationCost, `dao creation call should cost up to ${expectedDaoCreationCost} gas`)
+
+          const totalCost = tokenCreationCost + daoCreationCost
+          assert.isAtMost(totalCost, expectedTotalCost, `total costs should be up to ${expectedTotalCost} gas`)
+        })
+      }
+
+      const createDAO = () => {
+        before('create entity', async () => {
+          daoID = randomId()
+          tokenReceipt = await template.newToken(TOKEN_NAME, TOKEN_SYMBOL, { from: owner })
+          instanceReceipt = await newInstance(daoID, { from: owner })
+          await loadDAO(tokenReceipt, instanceReceipt)
+        })
+      }
+
+      createDAO()
+      itCostsUpTo(5e6)
+      itSetupsDAOCorrectly()
+    })
   })
 })
