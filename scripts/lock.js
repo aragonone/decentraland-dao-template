3f9ade23cc08d6e3d2a698d1c3df13c01444f52b
Alejandro J. Santander <palebluedot@gmail.com>
2019-09-10 10:49:20 -0300

    Improved lock script

--- scripts/lock.js
+++ scripts/lock.js
@@ -1,29 +1,32 @@
 /* global web3 artifacts */
 
+const AppProxyUpgradeable = artifacts.require('AppProxyUpgradeable')
 const TokenWrapper = artifacts.require('TokenWrapper')
 const Mana = artifacts.require('ERC20')
 const MiniMeToken = artifacts.require('MiniMeToken')
 
+console.log(`WARNING: This script is only intended to run in rinkeby right now...`)
+
 async function run() {
   const user = web3.currentProvider.address
-  console.log(`user:`, user)
+  console.log(`user address:`, user)
 
   const mana = Mana.at('0x28BcE5263f5d7F4EB7e8C6d5d78275CA455BAc63')
-  console.log(`mana:`, mana.address)
+  console.log(`mana address:`, mana.address)
 
   const proxyAddress = '0xE4D47793160d71C65Ab58c5A9dADA35C878AAcBc'
   const tokenWrapper = TokenWrapper.at(proxyAddress)
-  console.log(`tokenWrapper:`, tokenWrapper.address)
+  const proxy = AppProxyUpgradeable.at(proxyAddress)
+  const baseTokenWrapper = await proxy.implementation()
+  console.log(`tokenWrapper address (via proxy):`, tokenWrapper.address)
+  console.log(`tokenWrapper base implementation address:`, baseTokenWrapper)
 
   const orgTokenAddress = await tokenWrapper.token()
+  console.log(`org token address:`, orgTokenAddress)
   const orgToken = MiniMeToken.at(orgTokenAddress)
-  console.log(`org token:`, orgTokenAddress)
 
-  const orgTokenBalance = (await orgToken.balanceOf(user)).toNumber()
-  console.log(`user org token:`, orgTokenBalance)
-
-  const balance = ( await mana.balanceOf(user) ).toNumber()
-  console.log(`user mana:`, balance)
+  let balance = ( await mana.balanceOf(user) ).toNumber()
+  console.log(`user mana balance:`, balance)
 
   const allowance = ( await mana.allowance(user, tokenWrapper.address) ).toNumber()
   if (allowance === 0) {
@@ -39,6 +42,11 @@ async function run() {
 
   const lockedAmount = ( await tokenWrapper.getLockedAmount(user) ).toNumber()
   console.log(`Total locked amount for ${user}:`, lockedAmount)
+
+  const orgTokenBalance = (await orgToken.balanceOf(user)).toNumber()
+  balance = ( await mana.balanceOf(user) ).toNumber()
+  console.log(`user mana balance:`, balance)
+  console.log(`user org token balance:`, orgTokenBalance)
 }
 
 module.exports = callback => {
--- truffle.js
+++ truffle.js
@@ -1,2 +1,10 @@
-module.exports = require("@aragon/os/truffle-config")
+const config = require('@aragon/os/truffle-config')
 
+const HDWalletProvider = require('truffle-hdwallet-provider')
+// const MNEMONIC = 'stumble story behind hurt patient ball whisper art swift tongue ice alien'
+const MNEMONIC = 'another powder crowd summer tongue anxiety require multiply wise actor junk armed'
+config.networks.rinkeby.provider = function() {
+  return new HDWalletProvider(MNEMONIC, 'https://rinkeby.infura.io/v3/faf059296e4a41c28449a9cb95846c8d')
+}
+
+module.exports = config
